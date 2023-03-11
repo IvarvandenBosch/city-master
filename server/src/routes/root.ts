5747519eef2eb94aa868b58a95cdda89c1756ac2
Benjamin Lindberg <95551073+BenjaminLindberg@users.noreply.github.com>
2023-03-11 23:48:23 +0100

    feat: Configure fastify

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -1,9 +1,15 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    // Send back configuration
+    return {
+      name: process.env.NAME,
+      version: process.env.VERSION,
+      port: process.env.PORT,
+      enviroment: process.env.ENVIROMENT,
+    };
+  });
+};
 
 export default root;
--- server/test/routes/root.test.ts
+++ server/test/routes/root.test.ts
@@ -1,11 +1,16 @@
-import { test } from 'tap'
-import { build } from '../helper'
+import { test } from "tap";
+import { build } from "../helper";
 
-test('default root route', async (t) => {
-  const app = await build(t)
+test("default root route", async (t) => {
+  const app = await build(t);
 
   const res = await app.inject({
-    url: '/'
-  })
-  t.same(JSON.parse(res.payload), { root: true })
-})
+    url: "/",
+  });
+  t.same(JSON.parse(res.payload), {
+    name: process.env.NAME,
+    version: process.env.VERSION,
+    port: process.env.PORT,
+    enviroment: process.env.ENVIROMENT,
+  });
+});
